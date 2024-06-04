229fd749b3696a4015a43bbee58e90b300894a21
Luana Barros <dev.luana@gmail.com>
2024-06-04 14:16:03 -0300

    modifying dataset generator to support unique values only

--- datasetGenerator.cpp
+++ datasetGenerator.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <random>
 #include <numeric>
+#include <unordered_set>
 
 class DatasetGenerator {
 public:
@@ -29,6 +30,39 @@ public:
         return result;
     }
 
+    // std::vector<std::vector<int>> generateAlmostOrdered() {
+    //     std::vector<std::vector<int>> result;
+    //     std::random_device rd;
+    //     std::mt19937 g(rd());
+    //     for (int length : lengthLists) {
+    //         std::vector<int> half(length / 2);
+    //         std::iota(half.begin(), half.end(), 1);
+    //         std::vector<int> remaining(length - half.size());
+    //         std::iota(remaining.begin(), remaining.end(), half.size() + 1);
+    //         std::shuffle(remaining.begin(), remaining.end(), g);
+    //         half.insert(half.end(), remaining.begin(), remaining.end());
+    //         result.push_back(half);
+    //     }
+    //     return result;
+    // }
+
+    // std::vector<std::vector<int>> generateRandom() {
+    //     std::vector<std::vector<int>> result;
+    //     std::random_device rd;
+    //     std::mt19937 g(rd());
+    //     for (int length : lengthLists) {
+    //         std::vector<int> randomList(length);
+    //         std::generate(randomList.begin(), randomList.end(), [length]() {
+    //             static std::random_device rd;
+    //             static std::mt19937 g(rd());
+    //             static std::uniform_int_distribution<> dis(1, length * 2);
+    //             return dis(g);
+    //         });
+    //         result.push_back(randomList);
+    //     }
+    //     return result;
+    // }
+
     std::vector<std::vector<int>> generateAlmostOrdered() {
         std::vector<std::vector<int>> result;
         std::random_device rd;
@@ -39,7 +73,16 @@ public:
             std::vector<int> remaining(length - half.size());
             std::iota(remaining.begin(), remaining.end(), half.size() + 1);
             std::shuffle(remaining.begin(), remaining.end(), g);
-            half.insert(half.end(), remaining.begin(), remaining.end());
+            
+            // Verifica números únicos
+            std::unordered_set<int> uniqueNumbers(half.begin(), half.end());
+            for (int num : remaining) {
+                while (uniqueNumbers.count(num) > 0) {
+                    num = std::uniform_int_distribution<>(half.size() + 1, length)(g);
+                }
+                uniqueNumbers.insert(num);
+                half.push_back(num);
+            }
             result.push_back(half);
         }
         return result;
@@ -51,12 +94,17 @@ public:
         std::mt19937 g(rd());
         for (int length : lengthLists) {
             std::vector<int> randomList(length);
-            std::generate(randomList.begin(), randomList.end(), [length]() {
-                static std::random_device rd;
-                static std::mt19937 g(rd());
-                static std::uniform_int_distribution<> dis(1, length * 2);
-                return dis(g);
-            });
+            
+            // Verifica números únicos
+            std::unordered_set<int> uniqueNumbers;
+            for (int i = 0; i < length; ++i) {
+                int num = std::uniform_int_distribution<>(1, length * 2)(g);
+                while (uniqueNumbers.count(num) > 0) {
+                    num = std::uniform_int_distribution<>(1, length * 2)(g);
+                }
+                randomList[i] = num;
+                uniqueNumbers.insert(num);
+            }
             result.push_back(randomList);
         }
         return result;

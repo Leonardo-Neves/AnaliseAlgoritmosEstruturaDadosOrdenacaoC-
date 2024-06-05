7f1be254db8949b4780f6e98fc4e5a886c7ea106
Luana Barros <dev.luana@gmail.com>
2024-06-05 14:12:46 -0300

    Adding code with thread

--- main_with_threads.cpp
+++ main_with_threads.cpp
@@ -7,6 +7,8 @@
 #include <chrono>
 #include <fstream>
 #include <random>
+#include <thread>
+#include <mutex>
 
 #include "selectionSort.h"
 #include "quickSort.h"
@@ -20,9 +22,24 @@
 
 using namespace std;
 
-int main()
-{
-    std::vector<int> lengthLists = {10, 100};
+std::mutex csv_mutex;
+
+void runExperiment(int iteration, const std::string& algorithmName, const std::function<std::pair<std::vector<int>, std::pair<int, int>>(std::vector<int>)>& sortFunction, const std::vector<int>& dataset, const std::string& datasetName, std::string& csv) {
+    std::clock_t start = std::clock();
+    auto result = sortFunction(dataset);
+    std::clock_t end = std::clock();
+
+    long double diff = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+    std::string row = std::to_string(iteration + 1) + ";" + algorithmName + ";" + datasetName + ";" + std::to_string(dataset.size()) + ";" + std::to_string(diff) + ";" + std::to_string(result.second.first) + ";" + std::to_string(result.second.second) + "\n";
+
+    std::lock_guard<std::mutex> guard(csv_mutex);
+    csv += row;
+    // std::cout << "Unable to open file";
+}
+
+int main() {
+    std::vector<int> lengthLists = {10, 100, 1000, 10000};
     int NUMBER_INTERATIONS = 1;
 
     DatasetGenerator datasetGenerator(lengthLists);
@@ -39,7 +56,6 @@ int main()
     std::string csv = "Interation;Algorithm;DatasetName;DatasetSize;Time;Counter Comparisons;Counter Movements\n";
 
     for (int iteration = 0; iteration < NUMBER_INTERATIONS; ++iteration) {
-
         std::vector<std::vector<std::vector<int>>> datasets {
             datasetGenerator.generateOrdered(),
             datasetGenerator.generateOrderedInverse(),
@@ -54,22 +70,21 @@ int main()
             "Random"
         };
 
-        for (auto function = methods.begin(); function != methods.end(); ++function) {
+        std::vector<std::thread> threads;
 
+        for (auto function = methods.begin(); function != methods.end(); ++function) {
             for (int i = 0; i < datasets.size(); ++i) {
                 for (int j = 0; j < datasets[i].size(); ++j) {
-
-                    std::clock_t start = std::clock();
                     
-                    auto result = methods[function->first](datasets[i][j]);
-
-                    std::clock_t end = std::clock();
-
-                    long double diff = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-
-                    csv += std::to_string(iteration + 1) + ";" + function->first + ";" + datasets_name[i] + ";" + std::to_string(datasets[i][j].size()) + ";" + std::to_string(diff) + ";" + std::to_string(result.second.first) + ";" + std::to_string(result.second.second) + "\n";
-
+                    threads.push_back(std::thread(runExperiment, iteration, function->first, function->second, datasets[i][j], datasets_name[i], std::ref(csv)));
                 }
+            }
+        }
+
+        // Join all threads
+        for (std::thread &t : threads) {
+            if (t.joinable()) {
+                t.join();
             }
         }
     }
@@ -81,13 +96,10 @@ int main()
 
     std::ofstream file("output" + std::to_string(random_value) + ".csv");
 
-    if (file.is_open())
-    {
+    if (file.is_open()) {
         file << csv;
         file.close();
-    }
-    else 
-    {
+    } else {
         std::cout << "Unable to open file";
     }
 

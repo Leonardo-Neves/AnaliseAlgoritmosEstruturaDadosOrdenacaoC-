403c82575605e16cd4fee3bd5a091a5263bbc961
LeonardoNevesAeroespacial <leonardo.neves@aeroespacial.eng.br>
2024-06-08 12:51:55 -0300

    Save changes

---
+++ test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <map>
+#include <string>
+#include <utility>
+#include <chrono>
+#include <fstream>
+#include <random>
+#include <ctime>
+#include <mutex>
+#include <thread>
+#include <atomic>
+#include <iomanip>
+
+#include "datasetGenerator_v2.h"
+
+
+int main()
+{
+
+    std::vector<int> lengthLists = {10, 100, 1000, 10000, 100000, 1000000}
+
+    DatasetGenerator datasetGenerator;
+
+
+
+    return 0;
+}
